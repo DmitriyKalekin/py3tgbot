8f6936612564cc191a84fc6b899ecdf369b38079
Dmitriy Kalekin <herrhorror@gmail.com>
2020-11-27 02:41:28 +0600

    initial structure

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# py3tgbot
+# python3-telegram-bot-aiohttp
 Simple async python 3 telegram bot using fast aiohttp requests
+
+
+### Build package
+```
+python3 setup.py sdist bdist_wheel
+```
+
+
+### Docs
+Howto publish pypi package  
+https://medium.com/nuances-of-programming/python-%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D1%8F-%D0%B2%D0%B0%D1%88%D0%B8%D1%85-%D0%BF%D0%B0%D0%BA%D0%B5%D1%82%D0%BE%D0%B2-%D0%B2-pypi-11dd3216581c  
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+"""Setup script for python3-telegram-bot-aiohttp"""
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+requirements = ["aiohttp>=3.6.2"]
+
+setuptools.setup(
+	name="python3-telegram-bot-aiohttp",
+	version="0.0.1",
+	author="Dmitriy Kalekin",
+	author_email="herrhorror@gmail.com",
+	description="Simple async python 3 telegram bot using fast aiohttp requests",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/DmitriyKalekin/python3-telegram-bot-aiohttp",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3.8",
+		"License :: OSI Approved :: MIT",
+		"Operating System :: OS Independent",
+	],
+	install_requires=[
+		"aiohttp", "aioresponses",
+	],
+	python_requires='>=3.7',
+)
