169b833bcddfe336fdc171cb2de789e56c01f5d9
Dmitriy Kalekin <herrhorror@gmail.com>
2020-11-27 03:40:00 +0600

    readme change

--- build-package.sh
+++ build-package.sh
@@ -1,6 +1,6 @@
-rm -rf build
-rm -rf dist
-rm -rf py3tgbot.egg-info
+# rm -rf build
+# rm -rf dist
+# rm -rf py3tgbot.egg-info
 python3 setup.py sdist bdist_wheel
 twine upload --repository testpypi dist/*
 twine upload --repository pypi dist/*
