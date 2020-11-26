a4b2a9bc80198323585b009277f4040eff074eb2
Dmitriy Kalekin <herrhorror@gmail.com>
2020-11-27 02:42:12 +0600

    names in setup

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
 requirements = ["aiohttp>=3.6.2"]
 
 setuptools.setup(
-	name="python3-telegram-bot-aiohttp",
+	name="py3tgbot",
 	version="0.0.1",
 	author="Dmitriy Kalekin",
 	author_email="herrhorror@gmail.com",
 	description="Simple async python 3 telegram bot using fast aiohttp requests",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/DmitriyKalekin/python3-telegram-bot-aiohttp",
+	url="https://github.com/DmitriyKalekin/py3tgbot",
 	packages=setuptools.find_packages(),
 	classifiers=[
 		"Programming Language :: Python :: 3.8",
